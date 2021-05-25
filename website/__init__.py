f7c57ff11767deaea5af89d23b253456faf282bd
Brian-Bowyer <bobsnake64@gmail.com>
2021-05-25 01:18:22 -0500

    we dont need cross site scripting protection on a static site

--- website/__init__.py
+++ website/__init__.py
@@ -3,15 +3,12 @@ import os
 from flask import Flask, render_template
 # from flask_scss import Scss
 from flask_bootstrap import Bootstrap
-from flask_wtf.csrf import CSRFProtect
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
     Bootstrap(app)
     # Scss(app)
-    CSRFProtect(app)
 
     @app.route('/')
     def index():
